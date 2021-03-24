c61790876a7cdd1ba3083543e9497a745340589e
martinjakubec <martin.kubo8@gmail.com>
2021-03-24 21:13:26 +0100

    simplify schemas and modules

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,13 +1,37 @@
 const mongoose = require('mongoose');
 
-const HiveSchema = require('./HiveSchema')
-
 const UserSchema = new mongoose.Schema(
   {
     username: {type: String, unique: true, required: true},
     password: {type: String, required: true},
     email: {type: String, unique: true, required: true},
-    hives: [HiveSchema]
+    hives: [{
+      overalState: {type: String},
+      hiveNumber: {type: Number},
+      hiveColor: {type: String},
+      totalNumberOfFrames: {type: Number},
+      queenColor: {type: String},
+      queenNumber: {type: Number},
+      queenBreed: {type: String},
+      dateAdded: {type: Date},
+      honeySuper: {type: Number},
+      controls: [
+        {
+          dateOfControl: {type: Date},
+          numberOfFrames: {type: Number},
+          numberOfFullFrames: {type: Number},
+          numberOfFramesWithSealedBrood: {type: Number},
+          unsealedBrood: {type: Boolean},
+          queenSpotted: {type: Boolean},
+          freshEggs: {type: Boolean},
+          weather: {type: String},
+          temperature: {type: Number},
+          hostility: {type: Number},
+          workDone: {type: String},
+          workToDo: {type: [Object]}
+        }
+      ]
+    }],
   },
   {collection: 'users'}
 );
