0fe366e383881ea77e95626f29c7772655cab555
martinjakubec <martin.kubo8@gmail.com>
2021-03-26 23:39:40 +0100

    change UserModel to better update to dos

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -5,33 +5,37 @@ const UserSchema = new mongoose.Schema(
     username: {type: String, unique: true, required: true},
     password: {type: String, required: true},
     email: {type: String, unique: true, required: true},
-    hives: [{
-      overalState: {type: String},
-      hiveNumber: {type: Number},
-      hiveColor: {type: String},
-      totalNumberOfFrames: {type: Number},
-      queenColor: {type: String},
-      queenNumber: {type: Number},
-      queenBreed: {type: String},
-      dateAdded: {type: Date},
-      honeySuper: {type: Number},
-      controls: [
-        {
-          dateOfControl: {type: Date},
-          numberOfFrames: {type: Number},
-          numberOfFullFrames: {type: Number},
-          numberOfFramesWithSealedBrood: {type: Number},
-          unsealedBrood: {type: Boolean},
-          queenSpotted: {type: Boolean},
-          freshEggs: {type: Boolean},
-          weather: {type: String},
-          temperature: {type: Number},
-          hostility: {type: Number},
-          workDone: {type: String},
-          workToDo: {type: [Object]}
-        }
-      ]
-    }],
+    hives: [
+      {
+        overalState: {type: String},
+        hiveNumber: {type: Number},
+        hiveColor: {type: String},
+        totalNumberOfFrames: {type: Number},
+        queenColor: {type: String},
+        queenNumber: {type: Number},
+        queenBreed: {type: String},
+        dateAdded: {type: Date},
+        honeySuper: {type: Number},
+        controls: [
+          {
+            customId: {type: String},
+            dateOfControl: {type: Date},
+            numberOfFrames: {type: Number},
+            numberOfFullFrames: {type: Number},
+            numberOfFramesWithSealedBrood: {type: Number},
+            unsealedBrood: {type: Boolean},
+            queenSpotted: {type: Boolean},
+            freshEggs: {type: Boolean},
+            weather: {type: String},
+            temperature: {type: Number},
+            hostility: {type: Number},
+            workDone: {type: String},
+            workToDo: {type: String},
+            isToDoDone: {type: Boolean},
+          },
+        ],
+      },
+    ],
   },
   {collection: 'users'}
 );
