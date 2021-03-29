3b0d9d9e3ff30edf890e4939f03c2dddc7f1104a
martinjakubec <martin.kubo8@gmail.com>
2021-03-29 20:44:21 +0200

    initial front commit

---
+++ public/src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createI18n } from 'vue-i18n'
+
+import * as enLocale from './locales/en.json';
+import * as skLocale from './locales/sk.json';
+
+
+const messages = {
+  'English': enLocale.default,
+  'Slovak': skLocale.default
+}
+
+console.log(messages);
+
+const i18n = createI18n({
+  locale: 'English',
+  fallbackLocale: 'English',
+  messages
+})
+
+createApp(App).use(router).use(i18n).mount('#app')
