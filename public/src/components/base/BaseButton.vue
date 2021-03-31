9f00cc1e9c9901a416b0c0da98314de9dd4193b1
martinjakubec <martin.kubo8@gmail.com>
2021-03-31 22:10:33 +0200

    Add base components

--- public/src/components/base/BaseButton.vue
+++ public/src/components/base/BaseButton.vue
@@ -1,11 +1,61 @@
 <template>
-  <button class="button button-ghost">
+  <button @click="$emit('button-clicked')" class="button " :class="[isGhostButton ? 'button-ghost' : 'button-full']">
     <slot></slot>
   </button>
 </template>
 
 <script>
 export default {
-  props: ['class']
+  props: {
+    isGhostButton: {
+      type: Boolean,
+    }
+  },
+  emits: ['button-clicked']
 }
 </script>
+
+<style lang="scss" scoped>
+  .button {
+    margin: 10px;
+    padding: 10px 20px;
+    min-height: 50px;
+    min-width: 120px;
+    border-radius: 10px;
+    outline: none;
+    font-size: 2rem;
+    font-weight: bold;
+    box-sizing: border-box;
+
+    &:focus, &:hover {
+      outline: none;
+    }
+
+
+    &-full {
+      background-color: $dark-yellow;
+      border: none;
+      box-shadow: 0px 5px 10px 4px rgba(0, 0, 0, 0.25);
+      transition-property: background-color, box-shadow;
+      transition-duration: 0.15s;
+      transition-timing-function: ease-in-out;
+      &:hover, &:focus {
+        background-color: $darker-yellow;
+        box-shadow: 0px 5px 10px 4px rgba(0, 0, 0, 0.45);
+      }
+    }
+
+    &-ghost {
+      background-color: transparent;
+      border: 3px solid $dark-yellow;
+      color: $dark-yellow;
+      transition-property: background-color, color;
+      transition-duration: 0.15s;
+      &:hover, &:focus {
+        background-color: $dark-yellow;
+        color: $dark-gray;
+      }
+    }
+
+  }
+</style>
---
+++ public/src/components/base/BaseError.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="isModalVisible" class="error-modal">
+    <p>{{$t('error.errorTitle')}}</p>
+    <slot></slot>
+    <base-button @button-clicked="$emit('close-error')">Close</base-button>
+  </div>
+</template>
+
+<script>
+import BaseButton from './BaseButton.vue'
+export default {
+  components: { BaseButton },
+  props: {
+    isModalVisible: Boolean
+  },
+  emits: ['close-error']
+}
+</script>
+
+<style lang="scss" scoped>
+  .error-modal {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 999;
+    width: 400px;
+    min-height: 150px;
+    padding: 30px;
+    border: 3px solid $dark-yellow;
+    border-radius: 10px;
+    background-color: $lighter-gray;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    color: $dark-yellow;
+    
+  }
+</style>
