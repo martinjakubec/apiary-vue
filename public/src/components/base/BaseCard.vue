f2fcf9d46663ee5545c357cb63b04db5828ef21b
Martin Jakubec <martin.kubo8@gmail.com>
2021-05-07 22:50:43 +0200

    add card component

---
+++ public/src/components/base/BaseCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <slot></slot>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Base Card',
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border: 3px solid $dark-yellow;
+  background-color: $lighter-gray;
+  color: $dark-yellow;
+  padding: 2rem;
+  margin-right: 3rem;
+  margin-top: 3rem;
+  border-radius: 10px;
+  box-sizing: border-box;
+  width: 280px;
+  height: 340px;
+  transition-duration: 0.2s;
+  transition-property: transform;
+  position: relative;
+  overflow: hidden;
+
+  &:last-child {
+    margin-right: 0;
+  }
+
+  &:hover,
+  &:focus,
+  &:focus-within {
+    transform: translateY(-10px);
+    transition-duration: 0.2s;
+    transition-property: transform;
+  }
+}
+</style>
